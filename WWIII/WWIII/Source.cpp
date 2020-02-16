9d2ac7d6043cd4bbaabd421aa49fb2f5cbf98e30
Iliass Bourabaa <iliass.bourabaa@usherbrooke.ca>
2020-02-16 16:55:54 -0500

    Classe Item et classe Shop
    J'ai fait l'ensemble des fonctions nécessaire pour les deux classes.
    Par contre, il reste à modifier certaines méthodes de la classe Shop quand
    les classes enfants de Item et la classe Personnage vont être implémenter.

---
+++ WWIII/WWIII/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Shop.h"
+
+using namespace std;
+
+void main(void) {
+	//Test de la classe Item
+	/*Item potion;
+	potion.setId(POTION);
+	potion.setName("vie");
+	potion.setPriceBuy(-2);
+	potion.setPriceSell(1000);
+
+	cout << potion.getId() << " " << potion.getName() << " " << potion.getPriceBuy() << " " << potion.getPriceSell() << endl;*/
+
+	//Test de la classe Shop
+	/*Shop magasin;
+	Item item;
+
+	cout << endl << endl;
+	magasin.showList();
+	cout << endl << endl;
+
+	item = magasin.buyItem("sniper");
+	cout << endl << endl;
+
+	magasin.showList();
+	cout << endl << endl;
+
+	item = magasin.buyItem("lol");*/
+
+	system("pause");
+}
