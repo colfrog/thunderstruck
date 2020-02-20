292138b66bc0e79632ef9fa9491860b5c7823a5a
Laurent Cimon <laurent@nilio.ca>
2020-02-20 15:02:11 -0500

    Fix le build, rajoute les tests de coordonnées

--- tests/shop.cc
+++ tests/shop.cc
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <Shop.h>
 
@@ -21,21 +22,21 @@ int main() {
 
 	cout << "Test du magasin cr??" << endl;
 
-	magasin.showList();
+	magasin.show_list();
 	cout << endl << endl;
 
 	cout << "Test d'acheter une arme du magasin" << endl;
 	arme = magasin.buy_weapon(arme_id);
 	cout << endl << endl;
 
-	magasin.showList();
+	magasin.show_list();
 	cout << endl << endl;
 
 	cout << "Test d'acheter une potion qui n'est pas dans le magasin" << endl;
 	potion = magasin.buy_potion(defense_id);
 	cout << endl << endl;
 
-	magasin.showList();
+	magasin.show_list();
 	cout << endl << endl;
 
 	cout << "Test d'acheter une potion du magasin" << endl;
@@ -44,7 +45,7 @@ int main() {
 	cout << "Test d'acheter une potion du magasin" << endl;
 	defense = magasin.buy_defense(defense_id);
 
-	magasin.showList();
+	magasin.show_list();
 	cout << endl << endl;
 
 	cout << "Test des items achet?s" << endl;
