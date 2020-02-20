7c290f85d164956f4d67dd20a0870c3f7d2d5947
Laurent Cimon <laurent@nilio.ca>
2020-02-20 15:00:32 -0500

    Ajouter la classe Coord

---
+++ libWWIII/Coord.h
@@ -0,0 +1,21 @@
+class Coord {
+public:
+	Coord(int x, int y) : m_x(x), m_y(y) {};
+	~Coord() {};
+
+	int x();
+	int y();
+	void set_coords(int x, int y);
+
+	void show();
+	int distance(const Coord &) const;
+
+	bool operator==(const Coord &) const;
+	Coord operator+(const Coord &) const;
+	Coord operator-(const Coord &) const;
+	Coord &operator=(const Coord &) const;
+
+private:
+	int m_x;
+	int m_y;
+};
