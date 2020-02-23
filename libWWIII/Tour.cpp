b8fe8a6742db9d1cd56e4026c9c861e7d83262fe
Iliass Bourabaa <iliass.bourabaa@usherbrooke.ca>
2020-02-23 16:15:32 -0500

    Bug fix et classe Tour
    J'ai rajouter pragma once pour les classe .h pour pas qu'il est d'erreur de redéfinition. De plus, j'ai fini les méthodes de bases de la classe Tour

--- libWWIII/Tour.cpp
+++ libWWIII/Tour.cpp
@@ -9,10 +9,14 @@ Tour::~Tour()
 
 }
 
-/*Player Tour::get_player() {
+Player Tour::get_player() {
 	return m_player;
 }
 
 void Tour::set_player(Player player) {
 	m_player = player;
-}*/
+}
+
+void Tour::step() {
+	m_player.step();
+}
