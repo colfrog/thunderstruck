321096019a089cfcc319ce1b8c9c7726fc93192b
Laurent Cimon <laurent@nilio.ca>
2020-02-24 15:52:36 -0500

    Ajout de la logique d'exécution dans la classe Jeu

--- libWWIII/Jeu.cpp
+++ libWWIII/Jeu.cpp
@@ -1,3 +1,6 @@
+#include <thread>
+#include <chrono>
+
 #include "Jeu.h"
 #include "Lane.h"
 
@@ -9,13 +12,35 @@ Jeu::Jeu(int niveau_depart, int niveau_max, int f) :
 	Niveau n = Niveau(m_niveau_depart, m_niveau_max);
 	m_niveau = std::make_shared<Niveau>(n);
 
-	m_tour = std::unique_ptr<Tour>(new Tour());
-
 	for (int i = 0; i < 4; i++)
-		m_elems.push_back(std::unique_ptr<Lane>(new Lane(DirTools::dirs[i])));
+		m_elems.push_back(new Lane(DirTools::dirs[i]));
 }
 
 Jeu::~Jeu() {
+	for (Element *elem : m_elems)
+		delete elem;
+}
+
+void Jeu::run(std::function<void()> callback) {
+	m_running = true;
+	while (m_running) {
+		for (Element *elem : m_elems)
+			elem->step();
+
+		callback();
+		m_tour->step();
+
+		std::this_thread::sleep_for(
+			std::chrono::microseconds(1000000/m_frequence_jeu));
+	}
+}
+
+bool Jeu::running() {
+	return m_running;
+}
+
+void Jeu::stop() {
+	m_running = false;
 }
 
 int Jeu::niveau_actuel() const {
@@ -28,4 +53,8 @@ void Jeu::set_niveau(int niveau) {
 
 void Jeu::prochain_niveau() {
 	m_niveau->prochain();
+}
+
+Tour &Jeu::tour() {
+	return *m_tour;
 }
