4bc2b58575b42c016db4e38e49733a77d1a7fb75
Iliass Bourabaa <iliass.bourabaa@usherbrooke.ca>
2020-04-10 19:51:30 -0400

    Création de la classe de base ShopTable
    J'ai créé la classe de base mais il manque l'héritage des classe parents de ShopObject et le slot pour updatele shop.

---
+++ qt_ui/ShopWidget/ShopTable.cpp
@@ -0,0 +1,73 @@
+#include "ShopTable.h"
+
+ShopTable::ShopTable(Jeu jeu*) {
+	m_table_potion = new QTableWidget(1,5);
+	list<QString> informations = { "Id","Nom","Prix d'achat", "Prix de vente", "Vie recuperee"};
+
+	int index = 0;
+	for (QString info : informations) {
+		m_table->setHorizontalHeaderItem(index, new QTableWidgetItem(info));
+		index++;
+	}
+
+	index = 0;
+	for (Potion p : jeu.tour().player().get_shop().get_list_potion()) {
+		m_table->setItem(index, 0, new QTableWidgetItem(QString::number(p.get_id())));
+		m_table->setItem(index, 1, new QTableWidgetItem(QString::fromStdString(p.get_name())));
+		m_table->setItem(index, 2, new QTableWidgetItem(QString::number(p.get_price_buy())));
+		m_table->setItem(index, 3, new QTableWidgetItem(QString::number(p.get_price_sell())));
+		m_table->setItem(index, 4, new QTableWidgetItem(QString::number(p.get_hp_restore())));
+		index++;
+	}
+
+
+	m_table_weapon = new QTableWidget(1, 5);
+	informations.clear();
+	informations = { "Id","Nom","Prix d'achat", "Prix de vente", "Attaque" };
+
+	index = 0;
+	for (QString info : informations) {
+		m_table->setHorizontalHeaderItem(index, new QTableWidgetItem(info));
+		index++;
+	}
+
+	index = 0;
+	for (Weapon w : jeu.tour().player().get_shop().get_list_weapon()) {
+		m_table->setItem(index, 0, new QTableWidgetItem(QString::number(p.get_id())));
+		m_table->setItem(index, 1, new QTableWidgetItem(QString::fromStdString()));
+		m_table->setItem(index, 2, new QTableWidgetItem(QString::number()));
+		m_table->setItem(index, 3, new QTableWidgetItem(QString::number()));
+		m_table->setItem(index, 4, new QTableWidgetItem(QString::number()));
+		index++;
+	}
+
+
+	m_table_defense = new QTableWidget(1, 5);
+	informations.clear();
+	informations = { "Id","Nom","Prix d'achat", "Prix de vente", "Armure" };
+
+	index = 0;
+	for (QString info : informations) {
+		m_table->setHorizontalHeaderItem(index, new QTableWidgetItem(info));
+		index++;
+	}
+
+	index = 0;
+	for (Defense d : jeu.tour().player().get_shop().get_list_defense()) {
+		m_table->setItem(index, 0, new QTableWidgetItem(QString::number(p.get_id())));
+		m_table->setItem(index, 1, new QTableWidgetItem(QString::fromStdString()));
+		m_table->setItem(index, 2, new QTableWidgetItem(QString::number()));
+		m_table->setItem(index, 3, new QTableWidgetItem(QString::number()));
+		m_table->setItem(index, 4, new QTableWidgetItem(QString::number()));
+		index++;
+	}
+}
+ShopTable::~ShopTable() {
+	delete m_table_potion;
+	delete m_table_weapon;
+	delete m_table_defense;
+}
+
+void ShopTable::shopUpdate() {
+
+}
